29aa4e223bacc5744abb2b3b6640266350c4d9f2
Nir Aroeti <naroeti@drivenets.com>
2026-01-13 11:08:22 +0200

    Add new student screen

---
+++ app/src/main/java/com/example/studentsapp/NewStudentActivity.kt
@@ -0,0 +1,39 @@
+package com.example.studentsapp
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class NewStudentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_student)
+
+        val nameInput = findViewById<EditText>(R.id.nameInput)
+        val idInput = findViewById<EditText>(R.id.idInput)
+        val phoneInput = findViewById<EditText>(R.id.phoneInput)
+        val addressInput = findViewById<EditText>(R.id.addressInput)
+        val checkedBox = findViewById<CheckBox>(R.id.checkedBox)
+        val saveBtn = findViewById<Button>(R.id.saveButton)
+        val cancelBtn = findViewById<Button>(R.id.cancelButton)
+
+        saveBtn.setOnClickListener {
+            val student = Student(
+                id = idInput.text.toString(),
+                name = nameInput.text.toString(),
+                phone = phoneInput.text.toString(),
+                address = addressInput.text.toString(),
+                isChecked = checkedBox.isChecked
+            )
+            StudentRepository.addStudent(student)
+            finish()
+        }
+
+        cancelBtn.setOnClickListener {
+            finish()
+        }
+    }
+}
